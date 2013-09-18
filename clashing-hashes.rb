1be061c811117d692586c4f190325289872088e0
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-18 10:03:47 -0500

    adds a form

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -4,9 +4,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-@name = 'Fremen'
-@naam = "Karinn"
-
+  @name = 'Fremen'
+  @naam = "Karinn"
+  @show_description = false
   erb :index
   
+end
+
+post '/twitter' do
+
+
 end
